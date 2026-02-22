470c74a3c344cf2b756e575c1e921f8a30812e79
Yash-2707 <yashvrathod2004@gmail.com>
2026-02-22 23:32:34 +0530

    vite changes for frontend deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,24 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { fileURLToPath } from 'url'
-import path from 'path'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  // Crucial: This tells Vite to prefix all asset paths with /static/
-  base: '/static/', 
+  base: '/',
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: path.resolve(__dirname, '../server/static/dist'),
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]',
-      },
-    },
-  },
 })
