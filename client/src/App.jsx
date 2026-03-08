c9951e87315ae26ad0fd370a928cb21aca63aaeb
Yash-2707 <yashvrathod2004@gmail.com>
2026-03-09 01:09:17 +0530

    Changes of Demo in Production for app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,34 @@
-import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import HomePage from './pages/Home';
-import LoginPage from './pages/LoginPage';
-import ContactPage from './pages/ContactPage';
-import EmployeeProfile from './pages/profile/EmployeeProfile';
-import EmployeeDashboard from './pages/Dashboard/EmployeeDashboard';
-import SGMProfile from './pages/profile/SGMProfile';
-import ClientManagement from './pages/ClientManagement';
-import ClientProjects from './pages/ClientProjects';
-import InternalTeamView from './pages/InternalTeamView';
-import ProjectDetails from './pages/ProjectDetails';
-import AdminProfile from './pages/profile/AdminProfile';
-import Createuser from './pages/createuser/Createuser';
-import ClientProfile from './pages/profile/ClientProfile';
-import HQEPLProfile from './pages/profile/HQEPLProfile';
-import StaffManagement from './pages/StaffManagement';
-import ActionPlanDashboard from './pages/ActionPlanDashboard';
-import WeeklyScore from './pages/WeeklyScore';
-import ExternalManagement from './pages/ExternalManagement';
-import MCTC from './pages/MCTC';
-import MandaysPlanning from './pages/MandaysPlanning';
-import VisitAgenda from './pages/VisitAgenda';
-import VisitAgendaList from './pages/VisitAgendaList';
-import DDTMEBasePage from './pages/DDTME/DDTMEBasePage';
-import DDTMETable from './pages/DDTME/DDTMETable';
-import DDTMERYG from './pages/DDTME/DDTMERYG';
-import DDFMSBasePage from './pages/DDTME/DDFMSBasePage';
-import DDFMS from './pages/DDTME/DDFMS';
-import Achievement from './pages/Achievement/Achievement';
+import HomePage from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import ContactPage from "./pages/ContactPage";
+import EmployeeProfile from "./pages/profile/EmployeeProfile";
+import EmployeeDashboard from "./pages/Dashboard/EmployeeDashboard";
+import SGMProfile from "./pages/profile/SGMProfile";
+import ClientManagement from "./pages/ClientManagement";
+import ClientProjects from "./pages/ClientProjects";
+import InternalTeamView from "./pages/InternalTeamView";
+import ProjectDetails from "./pages/ProjectDetails";
+import AdminProfile from "./pages/profile/AdminProfile";
+import Createuser from "./pages/createuser/Createuser";
+import ClientProfile from "./pages/profile/ClientProfile";
+import HQEPLProfile from "./pages/profile/HQEPLProfile";
+import StaffManagement from "./pages/StaffManagement";
+import ActionPlanDashboard from "./pages/ActionPlanDashboard";
+import WeeklyScore from "./pages/WeeklyScore";
+import ExternalManagement from "./pages/ExternalManagement";
+import MCTC from "./pages/MCTC";
+import VisitAgenda from "./pages/VisitAgenda";
+import VisitAgendaList from "./pages/VisitAgendaList";
+import DDTMEBasePage from "./pages/DDTME/DDTMEBasePage";
+import DDTMETable from "./pages/DDTME/DDTMETable";
+import DDTMERYG from "./pages/DDTME/DDTMERYG";
+import DDFMSBasePage from "./pages/DDTME/DDFMSBasePage";
+import DDFMS from "./pages/DDTME/DDFMS";
+import Achievement from "./pages/Achievement/Achievement";
+
 const App = () => {
   return (
     <Router>
@@ -38,14 +38,14 @@ const App = () => {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/contact" element={<ContactPage />} />
 
-        <Route path='/admin' element={<AdminProfile />} />
-        <Route path='/admin/createuser' element={<Createuser />} />
-        <Route path='/staff' element={<StaffManagement />} />
+        <Route path="/admin" element={<AdminProfile />} />
+        <Route path="/admin/createuser" element={<Createuser />} />
+        <Route path="/staff" element={<StaffManagement />} />
 
-        <Route path='/employee' element={<EmployeeProfile />} />
-        <Route path='/employeedashboard' element={<EmployeeDashboard />} />
+        <Route path="/employee" element={<EmployeeProfile />} />
+        <Route path="/employeedashboard" element={<EmployeeDashboard />} />
 
-        <Route path='/sgm' element={<SGMProfile />} />
+        <Route path="/sgm" element={<SGMProfile />} />
         <Route path="/clients" element={<ClientManagement />} />
         <Route path="/clients/:clientId/" element={<ClientProjects />} />
         <Route path="/clients/:clientId/internal-team" element={<InternalTeamView />} />
@@ -54,22 +54,22 @@ const App = () => {
 
         <Route path="/client" element={<ClientProfile />} />
         <Route path="/hqepl" element={<HQEPLProfile />} />
+
         <Route path="/ddtme" element={<DDTMEBasePage />} />
         <Route path="/ddtme/client/:clientId" element={<DDTMETable />} />
         <Route path="/ddtme/client/:clientId/ryg" element={<DDTMERYG />} />
+
         <Route path="/ddfms" element={<DDFMSBasePage />} />
         <Route path="/ddfms/client/:clientId" element={<DDFMS />} />
-    
 
+        <Route path="/projects/:projectId/actionplan" element={<ActionPlanDashboard />} />
+        <Route path="/weeklyscore" element={<WeeklyScore />} />
 
-        <Route path='/projects/:projectId/actionplan' element={<ActionPlanDashboard />} />
-        <Route path='/weekly-score' element={<WeeklyScore />} />
-        <Route path='/weeklyscore' element={<WeeklyScore />} />
-        <Route path='/mandays-planning' element={<MandaysPlanning />} />
-        <Route path = '/mctc' element = {<MCTC />} />
-        <Route path = '/visitagenda' element = {<VisitAgendaList />} />
-        <Route path = '/visitagenda/:clientId' element = {<VisitAgenda />} />
-        <Route path = '/achievement' element = {<Achievement />} />
+        <Route path="/mctc" element={<MCTC />} />
+        <Route path="/visitagenda" element={<VisitAgendaList />} />
+        <Route path="/visitagenda/:clientId" element={<VisitAgenda />} />
+
+        <Route path="/achievement" element={<Achievement />} />
 
       </Routes>
     </Router>
